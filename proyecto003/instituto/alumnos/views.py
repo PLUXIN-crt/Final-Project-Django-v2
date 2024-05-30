d4b60ead4c315850ba910ca69356dbbc5a3beb55
PLUXIN <83378368+PLUXIN-crt@users.noreply.github.com>
2024-05-30 10:16:57 -0400

    Add files via upload

---
+++ proyecto003/instituto/alumnos/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Alumno
+# Create your views here.
+def menu(request):
+    return render(request, 'plantillaBase.html', {})
+
+def index(request):
+    alumnos = Alumno.objects.all() # select * from alumno
+    context = {"alumnos":alumnos}
+    print(alumnos)
+    return render(request, 'index.html', context)
+
+def listarAlumnos(request):
+    # utilizar ORM de Django
+    # select * from alumno
+    alumnos = Alumno.objects.all()
+
+    context = {'alumnos': alumnos}
+
+    return render(request, 'listarAlumnos.html', context)
